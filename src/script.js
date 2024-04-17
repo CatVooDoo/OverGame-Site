a862785111c4e98b2058c7e3b79f05ab24a9cae8
KlVnMrT <96387518+CatVooDoo@users.noreply.github.com>
2024-04-17 17:58:23 +0300

    Update script.js

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const sliderItem1 = document.querySelector(".item1");
+const sliderItem2 = document.querySelector(".item2");
+const sliderItem3 = document.querySelector(".item3");
+
+let items = [sliderItem1, sliderItem2, sliderItem3];
+let currentItem = 0;
+
+function slider() {
+    setInterval(function () {
+        items[currentItem].style.display = "none";
+        currentItem = (currentItem + 1) % items.length;
+        items[currentItem].style.display = "block";
+    }, 10000)
+}
+
+slider(); 

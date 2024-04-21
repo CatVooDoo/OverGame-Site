75765869d595cb3f0fd9f29a3ea648d14992b9ae
KlVnMrT <96387518+CatVooDoo@users.noreply.github.com>
2024-04-21 19:27:52 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,32 @@
 let productCards = document.querySelectorAll(".catalog__item");
+const modalWindow = document.getElementById("modal");
 
 productCards.forEach(function (card) {
     card.addEventListener('mouseover', function () {
         card.classList.add('catalog__item-anim');
-        console.log('класс добавлен');
     });
 
     card.addEventListener('mouseout', function () {
         card.classList.remove('catalog__item-anim');
-        console.log('класс удален');
+    });
+
+    card.addEventListener('click', function () {
+        card.classList.add('catalog__item-click');
+        modalWindow.style.display = "flex"; // Изменить значение display для показа модального окна
     });
 });
+
+// Добавить обработчик события для закрытия модального окна
+
+window.addEventListener('click', function (event) {
+    if (event.target === modalWindow) {
+        modalWindow.style.display = "none"; // Скрыть модальное окно при клике вне его области
+        productCards.forEach(function (card) {
+            card.addEventListener('click', function () {
+                card.classList.remove('catalog__item-click');
+            });
+        });
+    }
+});
+
+// 🅺🅻🆅🅽🅼🆁🆃
